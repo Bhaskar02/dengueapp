129b3a2eba44ec8caeafb5fe3853455131676464
Bhaskar02 <bhaskarrao02@gmail.com>
2021-11-02 10:01:34 +0530

    Update heatmap.py

--- apps/heatmap.py
+++ apps/heatmap.py
@@ -7,7 +7,7 @@ def app():
     st.title('Heatmaps')
 
     filepath = "https://raw.githubusercontent.com/Bhaskar02/dengueapp/main/data/dw.csv"
-    m = leafmap.Map(tiles="stamentoner")
+    m = leafmap.Map(location=[20.5937, 78.9629],zoom_start=6,tiles="stamentoner")
     m.add_heatmap(
         filepath,
         latitude="lat",
