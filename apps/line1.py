e4a7ce4b44e22644351a22529807956ded284cd6
Bhaskar02 <bhaskarrao02@gmail.com>
2021-11-10 10:46:47 +0530

    Update line1.py

--- apps/line1.py
+++ apps/line1.py
@@ -6,7 +6,7 @@ import leafmap.foliumap as leafmap
 
 def app():
   st.title('BarGraph')
-  df1 = pd.read_csv('dengue101721f.csv')
+  df1 = pd.read_csv('https://github.com/Bhaskar02/dengueapp/blob/main/data/dengue101721f.csv')
   #df = df.groupby(['States/UTs', '2012'])
   columns1 = df1.columns.tolist()
   selected_columns = st.multiselect("select column", columns1, default='2012')
