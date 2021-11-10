a4ff415cf4695c144ee5a761c47f9ef41237744c
Bhaskar02 <bhaskarrao02@gmail.com>
2021-11-10 10:13:18 +0530

    Update line1.py

--- apps/line1.py
+++ apps/line1.py
@@ -2,15 +2,18 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 import plotly.graph_objs as go
+import leafmap.foliumap as leafmap
 
-df1 = pd.read_csv('E:/dengue data python/leafmap/data/dengue101721f.csv')
-#df = df.groupby(['States/UTs', '2012'])
-columns1 = df1.columns.tolist()
-selected_columns = st.multiselect("select column", columns1, default='2012')
-s = df1[selected_columns[0]]
+def app():
+  st.title('BarGraph')
+  df1 = pd.read_csv('E:/dengue data python/leafmap/data/dengue101721f.csv')
+  #df = df.groupby(['States/UTs', '2012'])
+  columns1 = df1.columns.tolist()
+  selected_columns = st.multiselect("select column", columns1, default='2012')
+  s = df1[selected_columns[0]]
 
-trace = go.Bar(x=df1['States/UTs'],y=s.values,showlegend = True)
-layout = go.Layout(title = "test")
-data = [trace]
-fig = go.Figure(data=data,layout=layout)
-st.plotly_chart(fig)
+  trace = go.Bar(x=df1['States/UTs'],y=s.values,showlegend = True)
+  layout = go.Layout(title = "test")
+  data = [trace]
+  fig = go.Figure(data=data,layout=layout)
+  st.plotly_chart(fig)
