211ac8087e653fc9644bb93ac14dd5ce9ec9a228
Bhaskar02 <bhaskarrao02@gmail.com>
2021-11-12 11:48:17 +0530

    Update heatmapf.py

--- apps/heatmapf.py
+++ apps/heatmapf.py
@@ -11,8 +11,8 @@ def app():
     url = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv'
     df1 = pd.read_csv(url)
     columns1 = df1.columns.tolist()
-    selected_columns = st.multiselect("select column", columns1, default='2012')
-    s = df1[selected_columns[0]]
+    #selected_columns = st.multiselect("select column", columns1, default='2012')
+    #s = df1[selected_columns[0]]
     m = leafmap.Map(tiles="Heatmap")
     m.add_heatmap(
         filepath,
