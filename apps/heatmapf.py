1138eaa15963522effa5a3ea6bd81d754218229a
Bhaskar02 <bhaskarrao02@gmail.com>
2021-11-12 11:50:35 +0530

    Update heatmapf.py

--- apps/heatmapf.py
+++ apps/heatmapf.py
@@ -11,6 +11,9 @@ def app():
     st.title('Heatmaps')
 
     filepath = "https://raw.githubusercontent.com/Bhaskar02/dengueapp/main/data/dw.csv"
+    url = 'https://raw.githubusercontent.com/Bhaskar02/dengueapp/main/data/denguef.csv'
+    df1 = pd.read_csv(url)
+    columns1 = df1.columns.tolist()
     m = leafmap.Map(location=[20.5937, 78.9629],zoom_start=5,tiles="stamentoner")
     m.add_heatmap(
         filepath,
