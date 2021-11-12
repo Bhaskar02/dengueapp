76ffaa911e4a23f18a013c42181dad7d9117e7c3
Bhaskar02 <bhaskarrao02@gmail.com>
2021-11-12 11:49:18 +0530

    Update heatmapf.py

--- apps/heatmapf.py
+++ apps/heatmapf.py
@@ -4,22 +4,20 @@ import plotly.express as px
 import plotly.graph_objs as go
 import leafmap.foliumap as leafmap
 
+
+
 def app():
 
-    st.title('Heatmaps1')
-    filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
-    url = 'https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv'
-    df1 = pd.read_csv(url)
-    columns1 = df1.columns.tolist()
-    #selected_columns = st.multiselect("select column", columns1, default='2012')
-    #s = df1[selected_columns[0]]
-    m = leafmap.Map(tiles="Heatmap")
+    st.title('Heatmaps')
+
+    filepath = "https://raw.githubusercontent.com/Bhaskar02/dengueapp/main/data/dw.csv"
+    m = leafmap.Map(location=[20.5937, 78.9629],zoom_start=5,tiles="stamentoner")
     m.add_heatmap(
         filepath,
         latitude="lat",
-        longitude="long",
-        value="2012",
+        longitude="lon",
+        value="cases",
         name="Heat map",
         radius=20,
     )
-    m.to_streamlit(width=700, height=500)
+    m.to_streamlit(width=700, height=700)
