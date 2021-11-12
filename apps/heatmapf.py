34ec1f429e3c9c12ee9430c19c551678f99a6f6a
Bhaskar02 <bhaskarrao02@gmail.com>
2021-11-12 11:34:22 +0530

    Add files via upload

---
+++ apps/heatmapf.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+import plotly.graph_objs as go
+import leafmap.foliumap as leafmap
+
+def app():
+
+    st.title('Heatmaps1')
+
+    filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
+    df1 = pd.read_csv(filepath)
+    columns1 = df1.columns.tolist()
+    selected_columns = st.multiselect("select column", columns1, default='2012')
+    s = df1[selected_columns[0]]
+    m = leafmap.Map(tiles="stamentoner")
+    m.add_heatmap(
+        filepath,
+        latitude="latitude",
+        longitude="longitude",
+        value=s.values,
+        name="Heat map",
+        radius=20,
+    )
+    m.to_streamlit(width=700, height=500)
