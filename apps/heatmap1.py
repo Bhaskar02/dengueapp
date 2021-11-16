f40c9045477a0c5ca5e5d40c25364c0a3e8724a1
Bhaskar02 <bhaskarrao02@gmail.com>
2021-11-16 10:31:12 +0530

    Update and rename heatmap.py to heatmap1.py

--- apps/heatmap1.py
+++ apps/heatmap1.py
@@ -4,7 +4,7 @@ import leafmap.foliumap as leafmap
 
 def app():
 
-    st.title('Heatmaps')
+    st.title('Heatmaps1')
 
     filepath = "https://raw.githubusercontent.com/Bhaskar02/dengueapp/main/data/dw.csv"
     m = leafmap.Map(location=[20.5937, 78.9629],zoom_start=5,tiles="stamentoner")
