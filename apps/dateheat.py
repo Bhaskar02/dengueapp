b01d8135b18f2e7ecc029249eac370e0f9d525bc
Bhaskar02 <bhaskarrao02@gmail.com>
2021-11-16 10:03:35 +0530

    Update dateheat.py

--- apps/dateheat.py
+++ apps/dateheat.py
@@ -21,6 +21,6 @@ def app():
     tomorrow = today + datetime.timedelta(days=1)
     start_date = st.date_input('Start date', today)
     end_date = st.date_input('End date', tomorrow)
-    data=df[(df.dat>=start_date) & (df.dat<=end_date)]
-    data=data[['lat','lon','cases']]
+    //data=df[(df.dat>=start_date) & (df.dat<=end_date)]
+    //data=data[['lat','lon','cases']]
     m.to_streamlit(width=700, height=700)
