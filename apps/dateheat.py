e6025b3ab8c84e213abb711648a71558818af04c
Bhaskar02 <bhaskarrao02@gmail.com>
2021-11-16 10:05:03 +0530

    Update dateheat.py

--- apps/dateheat.py
+++ apps/dateheat.py
@@ -14,13 +14,11 @@ def app():
     
     
     df = pd.read_csv('https://raw.githubusercontent.com/Bhaskar02/dengueapp/main/data/dengue11f.csv')
-    data=df[(df.dat>='1-1-2019') & (df.dat<='11-11-2021')]
-    data=data[['lat','lon','cases']]
+    
     m = leafmap.Map(location=[20.5937, 78.9629],zoom_start=5,tiles="stamentoner")
     today = datetime.date.today()
     tomorrow = today + datetime.timedelta(days=1)
     start_date = st.date_input('Start date', today)
     end_date = st.date_input('End date', tomorrow)
-    //data=df[(df.dat>=start_date) & (df.dat<=end_date)]
-    //data=data[['lat','lon','cases']]
+    
     m.to_streamlit(width=700, height=700)
