1dbcc70874d588081c52b68d96d3fddc58982adf
Bhaskar02 <bhaskarrao02@gmail.com>
2022-01-24 10:59:21 +0530

    Update bhas1.py

--- bhas1.py
+++ bhas1.py
@@ -2,7 +2,7 @@ import streamlit as st
 from multiapp import MultiApp
 from apps import (
     
-    h1,
+    
     line1,
     line,
     heatmapf,
@@ -19,7 +19,7 @@ apps = MultiApp()
 # Add all your application here
 
 
-apps.add_app("Hmaps", h1.app)
+
 apps.add_app("Bargraph", line1.app)
 apps.add_app("Bargraph1", line.app)
 apps.add_app("heatmapf", heatmapf.app)
