27761405c67d9e5d632107429e8c3441902cb9d7
Bhaskar02 <bhaskarrao02@gmail.com>
2022-02-04 15:42:30 +0530

    Update heatmapf.py

--- apps/heatmapf.py
+++ apps/heatmapf.py
@@ -14,9 +14,9 @@ def app():
     url = 'https://raw.githubusercontent.com/Bhaskar02/dengueapp/main/data/denguelatdata.csv'
     df1 = pd.read_csv(url)
     columns1 = df1.columns.tolist()
-    #selected_columns = st.multiselect("select column", columns1, default='2012')
+    #selected_columns = st.multiselect("select column", columns1, default='2012')selected_columns[0]
     selected_columns =st.selectbox('Select the year',('2010', '2011', '2012','2013','2014','2015','2016','2017','2018'))
-    filepath = "https://raw.githubusercontent.com/Bhaskar02/dengueapp/main/data/"+selected_columns[0]+".csv"
+    filepath = "https://raw.githubusercontent.com/Bhaskar02/dengueapp/main/data/"+selected_columns+".csv"
     s = df1[selected_columns[0]]
     m = leafmap.Map(location=[20.5937, 78.9629],zoom_start=5,tiles="stamentoner")
     m.add_heatmap(
